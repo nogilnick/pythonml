5c095f2341d8396580230d95c1b5713e48573130
Nicholas Smith <nicholastoddsmith@gmail.com>
2018-02-24 19:28:50 -0800

    Updated OCR generation script

--- DeepOCR/DeepOCR.py
+++ DeepOCR/DeepOCR.py
@@ -1,4 +1,5 @@
 import numpy as np
+import argparse
 import os
 import string
 import sys
@@ -16,6 +17,35 @@ def DivideIntoSubimages(I):
     WP = IS[1] * W
     I = I[0:HP, 0:WP]     #Discard any extra pixels
     return I.reshape(H, IS[0], -1, IS[1], c).swapaxes(1, 2).reshape(-1, IS[0], IS[1], c)
+    
+def FitModel():
+    print('Fitting model...')
+    A, Y, T, FN = LoadData()
+    ss = ShuffleSplit(n_splits = 1, random_state = 42)
+    trn, tst = next(ss.split(A))
+    #Fit the network
+    cnnc.fit(A[trn], Y[trn])
+    #The predictions as sequences of character indices
+    YH = []
+    for i in np.array_split(np.arange(A.shape[0]), 32): 
+        YH.append(cnnc.predict(A[i]))
+    YH = np.vstack(YH)
+    #Convert from sequence of char indices to strings
+    PS = np.array([''.join(YHi) for YHi in YH])
+    #Compute the accuracy
+    S1 = SAcc(PS[trn], T[trn])
+    S2 = SAcc(PS[tst], T[tst])
+    print('Train: ' + str(S1))
+    print('Test: ' + str(S2))
+    for PSi, Ti, FNi in zip(PS, T, FN):
+        if np.random.rand() > 0.99: #Randomly select rows to print
+            print(FNi + ': ' + Ti + ' -> ' + PSi)
+    print('Fitting CV data...')
+    #Fit remainder
+    cnnc.fit(A[tst], Y[tst])
+    cnnc.SaveModel(os.path.join('TFModel', 'ocrnet'))
+    with open('TFModel/_classes.txt', 'w') as F:
+        F.write('\n'.join(cnnc._classes))
     
 def ImageToString(I):
     '''
@@ -40,13 +70,13 @@ def LoadData(FP = '.'):
     FP:     Path to OCR data folder
     return: Data Matrix, Target Matrix, Target Strings
     '''
-    TFP = os.path.join(FP, 'Train.csv')
+    TFP = os.path.join(FP, 'Trn.csv')
     A, Y, T, FN = [], [], [], []
     with open(TFP) as F:
         for Li in F:
             FNi, Yi = Li.strip().split(',')                     #filename,string
             T.append(Yi)
-            A.append(imread(os.path.join(FP, 'Out', FNi)))
+            A.append(imread(os.path.join(FP, FNi))[:, :, :3])   #Read image and discard alpha channel
             Y.append(list(Yi) + [' '] * (MAX_CHAR - len(Yi)))   #Pad strings with spaces
             FN.append(FNi)
     return np.stack(A), np.stack(Y), np.stack(T), np.stack(FN)
@@ -63,43 +93,28 @@ def SubimageShape(I):
 
 NC = len(string.ascii_letters + string.digits + ' ')
 MAX_CHAR = 64
-IS = (14, 640, 3)       #Image size for CNN
+IS = (18, 640, 3)       #Image size for CNN
 #Architecture of the neural network
 ws = [('C', [4, 4,  3, NC // 2], [1, 2, 2, 1]), ('AF', 'relu'), 
       ('C', [4, 4, NC // 2, NC], [1, 2, 1, 1]), ('AF', 'relu'), 
       ('C', [8, 5, NC, NC], [1, 8, 5, 1]), ('AF', 'relu'),
       ('R', [-1, 64, NC])]
 #Create the neural network in TensorFlow
-cnnc = ANNC(IS, ws, batchSize = 64, learnRate = 5e-5, maxIter = 32, reg = 1e-5, tol = 1e-2, verbose = True)
+cnnc = ANNC(IS, ws, batchSize = 128, learnRate = 5e-5, maxIter = 48, reg = 1e-5, tol = 1e-2, verbose = True)
 if not cnnc.RestoreModel('TFModel/', 'ocrnet'):
-    A, Y, T, FN = LoadData()
-    ss = ShuffleSplit(n_splits = 1, random_state = 42)
-    trn, tst = next(ss.split(A))
-    #Fit the network
-    cnnc.fit(A[trn], Y[trn])
-    #The predictions as sequences of character indices
-    YH = []
-    for i in np.array_split(np.arange(A.shape[0]), 32): 
-        YH.append(cnnc.predict(A[i]))
-    YH = np.vstack(YH)
-    #Convert from sequence of char indices to strings
-    PS = np.array([''.join(YHi) for YHi in YH])
-    #Compute the accuracy
-    S1 = SAcc(PS[trn], T[trn])
-    S2 = SAcc(PS[tst], T[tst])
-    print('Train: ' + str(S1))
-    print('Test: ' + str(S2))
-    for PSi, Ti, FNi in zip(PS, T, FN):
-        print(FNi + ': ' + Ti + ' -> ' + PSi)
-    cnnc.SaveModel(os.path.join('TFModel', 'ocrnet'))
-    with open('TFModel/_classes.txt', 'w') as F:
-        F.write('\n'.join(cnnc._classes))
+    FitModel()
 else:
     with open('TFModel/_classes.txt') as F:
         cnnc.RestoreClasses(F.read().splitlines())
     
 if __name__ == "__main__":
-    for img in sys.argv[1:]:
-        I = imread(img)
+    P = argparse.ArgumentParser(description = 'Deep learning based OCR')
+    P.add_argument('-f', action = 'store_true', help = 'Force model training')
+    P.add_argument('Img', metavar = 'I', type = str, nargs = '+', help = 'Image files')
+    PA = P.parse_args()
+    if PA.f:
+        FitModel()
+    for img in PA.Img:
+        I = imread(img)[:, :, :3]    #Read image and discard alpha
         S = ImageToString(I)
         print(S)
--- DeepOCR/OCRGen.py
+++ DeepOCR/OCRGen.py
@@ -3,52 +3,83 @@ from numpy.random import randint, choice
 import os
 import string
 from PIL import Image, ImageFont, ImageDraw
-
-TF = ImageFont.truetype('consola.ttf', 18)
-
-def MakeImg(t, f, fn, s = (100, 100), o = (0, 0)):
+import matplotlib.pyplot as mpl
+    
+def GenBlock(MINC, MAXC, s = (100, 100), o = (0, 0)):
     '''
-    Generate an image of text
-    t:      The text to display in the image
-    f:      The font to use
-    fn:     The file name
-    s:      The image size
-    o:      The offest of the text in the image
+    Generates a single image block. This is the image size the CNN uses
     '''
+    Si = ''.join(choice(CS, randint(MINC, MAXC)))
     img = Image.new('RGB', s, "black")
     draw = ImageDraw.Draw(img)
-    draw.text(o, t, (255, 255, 255), font = f)
-    img.save(fn)
-    
-def GetFontSize(S):
+    draw.text(o, Si, (255, 255, 255), font = TF)
+    return np.array(img), Si
+        
+def GenImage(NR, NC, IS, MINC = 10, MAXC = 64, NIMG = 128, DP = 'Trn'):
+    '''
+    NR:   Number of rows (blocks)
+    NC:   Number of columns (blocks)
+    IS:   Image size
+    MINC: Minimum number of characters per line
+    MAXC: Maximum number of characters per line
+    NIMG: Number of images to generate
+    DP:   Directory path to write images
+    '''
+    Y = []
+    MS = GetFontSize(MAXC)
+    NB = NR * NC #Number of blocks total = Rows * Cols
+    for i in range(NIMG):               #Write images to ./Out/ directory
+        Im, Ym = MergeBlock(NR, NC, [GenBlock(MINC, MAXC, IS) for _ in range(NB)])
+        FNi = os.path.join(DP, '{:05d}.png'.format(i))
+        mpl.imsave(FNi, Im)
+        Y.append(FNi + ',' + Ym)
+    with open(DP + '.csv', 'w') as F:   #Write CSV file
+        F.write('\n'.join(Y))
+   
+def GetFontSize(MAXC):
+    '''
+    Gets the maximum size of an image containing characters in CS
+    of maximum length MAXC
+    '''
     img = Image.new('RGB', (1, 1), "black")
     draw = ImageDraw.Draw(img)
-    return draw.textsize(S, font = TF)
+    h, w = 0, 0
+    for CSi in CS:  #Get max height and width possible characters CS
+        tsi = draw.textsize(CSi * MAXC, font = TF) 
+        h = max(tsi[0], h)
+        w = max(tsi[1], w)
+    return (h, w)   
+    
+def MergeBlock(NR, NC, T):
+    '''
+    Merges blocks into combined images that are NR blocks tall and NC blocks wide
+    NR:  Number of rows (blocks)
+    NC:  Number of columns (blocks)
+    T:   List of outputs from GenBlock
+    ret: Merged image, Merged string
+    '''
+    B = np.array([t[0] for t in T])
+    Y = np.array([t[1] for t in T])
+    n, r, c, _ = B.shape
+    return Unblock(B, r * NR, c * NC), '@'.join(''.join(Yi) for Yi in Y.reshape(NR, NC))
+     
+def Unblock(I, h, w):
+    '''
+    I:   Array of shape (n, NR, NC, c)
+    h:   Height of new array
+    w:   Width of new array
+    ret: Array of shape (h, w, c)
+    '''
+    n, NR, NC, c = I.shape
+    return I.reshape(h // NR, -1, NR, NC, c).swapaxes(1, 2).reshape(h, w, c)
+       
+TF = ImageFont.truetype('consola.ttf', 18)
+#Possible characters to use
+CS = list(string.ascii_letters + string.digits + ' ')
 
-def GenSingleLine(MINC = 10, MAXC = 64, NIMG = 128, DP = 'Out'):               #Font path
-    #The possible characters to use
-    CS = list(string.ascii_letters) + list(string.digits)
-    MS = GetFontSize('\n'.join(['0' * MAXC]))   #Size needed to fit MAXC characters
-    Y = []
-    for i in range(NIMG):               #Write images to ./Out/ directory
-        Si = ''.join(choice(CS, randint(MINC, MAXC)))
-        FNi = str(i) + '.png'
-        MakeImg(Si, TF, os.path.join(DP, FNi), MS)
-        Y.append(FNi + ',' + Si)
-    with open('Train.csv', 'w') as F:   #Write CSV file
-        F.write('\n'.join(Y))
-        
-def GenMultiLine(ML = 5, MINC = 10, MAXC = 64, NIMG = 128, DP = 'Img'):
-    #The possible characters to use
-    CS = list(string.ascii_letters) + list(string.digits)
-    MS = GetFontSize('\n'.join(ML * ['0' * MAXC]))
-    Y = []
-    for i in range(NIMG):               #Write images to ./Out/ directory
-        Si = '\n'.join(''.join(choice(CS, randint(MINC, MAXC))) for _ in range(randint(1, ML + 1)))
-        FNi = str(i) + '.png'
-        MakeImg(Si, TF, os.path.join(DP, FNi), MS)
-        Y.append(FNi + ',' + Si.replace('\n', '@'))
-        
 if __name__ == "__main__":
-    #GenMultiLine(ML = 1, MAXC = 128)
-    GenSingleLine(NIMG = 1024)
+    nc, xc = 10, 64
+    ms = GetFontSize(xc)
+    print('CNN Image Size: ' + str(ms))
+    GenImage(1, 1, ms, nc, xc, NIMG = 9999, DP = 'Trn') #Training data
+    GenImage(4, 2, ms, nc, xc, NIMG = 256,  DP = 'Tst') #Testing data
