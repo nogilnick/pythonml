c6989c958de42d2b29c48754b6f656c5eab92e16
Nicholas Smith <nicholastoddsmith@gmail.com>
2018-02-27 19:52:27 -0800

    Make deep OCR model use a bagging approach

--- DeepOCR/DeepOCR.py
+++ DeepOCR/DeepOCR.py
@@ -6,6 +6,16 @@ import sys
 from skimage.io import imread
 from sklearn.model_selection import ShuffleSplit
 from TFANN import ANNC
+import tensorflow as tf
+from scipy.stats import mode as Mode
+
+NC = len(string.ascii_letters + string.digits + ' ')    #Number of possible characters
+MAX_CHAR = 64           #Max # characters per block
+IS = [18, 640, 3]       #Image size for CNN
+NNET = 5                #Use 5 networks with a bagging approach
+#Shared placeholders for images and target data
+TFIM = tf.placeholder("float", [None] + IS, name = 'TFIM') 
+TFYH = tf.placeholder("float", [None, MAX_CHAR, NC], name = 'TFYH')
 
 def DivideIntoSubimages(I):
     '''
@@ -18,10 +28,20 @@ def DivideIntoSubimages(I):
     I = I[0:HP, 0:WP]     #Discard any extra pixels
     return I.reshape(H, IS[0], -1, IS[1], c).swapaxes(1, 2).reshape(-1, IS[0], IS[1], c)
     
-def FitModel():
+def MakeNet(nn = 'ocrnet'):
+    #Architecture of the neural network
+    #The input volume is reduce to the shape of the output in conv layers
+    #18 / 2 * 3 * 3 = 1 and 640 / 2 * 5 = 64 output.shape
+    ws = [('C', [5, 5,  3, NC // 2], [1, 2, 2, 1]), ('AF', 'relu'),     
+          ('C', [4, 4, NC // 2, NC], [1, 3, 1, 1]), ('AF', 'relu'), 
+          ('C', [3, 5, NC,      NC], [1, 3, 5, 1]), ('AF', 'relu'),
+          ('R', [-1, MAX_CHAR, NC])]
+    #Create the neural network in TensorFlow
+    return ANNC(IS, ws, batchSize = 512, learnRate = 2e-5, maxIter = 64, name = nn, reg = 1e-5, tol = 1e-2, verbose = True, X = TFIM, Y = TFYH)
+    
+def FitModel(cnnc, A, Y, T, FN):
     print('Fitting model...')
-    A, Y, T, FN = LoadData()
-    ss = ShuffleSplit(n_splits = 1, random_state = 42)
+    ss = ShuffleSplit(n_splits = 1)
     trn, tst = next(ss.split(A))
     #Fit the network
     cnnc.fit(A[trn], Y[trn])
@@ -44,18 +64,19 @@ def FitModel():
     #Fit remainder
     cnnc.SetMaxIter(4)
     cnnc.fit(A, Y)
-    cnnc.SaveModel(os.path.join('TFModel', 'ocrnet'))
-    with open('TFModel/_classes.txt', 'w') as F:
-        F.write('\n'.join(cnnc._classes))
+    return cnnc
+    
+def FuseResults(T): #Take mode result from 5 networks
+    return (Mode(np.stack([Ti.ravel() for Ti in T]))[0]).reshape(T[0].shape)
     
 def ImageToString(I):
     '''
     Uses OCR to transform an image into a string
     '''
     SI = DivideIntoSubimages(I)
-    YH = cnnc.predict(SI)
     ss = SubimageShape(I)
-    return JoinStrings(YH, ss)
+    YH = FuseResults(TFS.run(YHL, feed_dict = {TFIM: SI}))
+    return JoinStrings(CNN[-1]._classes[YH], ss)
     
 def JoinStrings(YH, ss):
     '''
@@ -74,7 +95,7 @@ def LoadData(FP = '.'):
     TFP = os.path.join(FP, 'Trn.csv')
     A, Y, T, FN = [], [], [], []
     with open(TFP) as F:
-        for Li in F:
+        for i, Li in enumerate(F):
             FNi, Yi = Li.strip().split(',')                     #filename,string
             T.append(Yi)
             A.append(imread(os.path.join(FP, FNi))[:, :, :3])   #Read image and discard alpha channel
@@ -92,24 +113,25 @@ def SubimageShape(I):
     h, w, c = I.shape
     return h // IS[0], w // IS[1]
 
-NC = len(string.ascii_letters + string.digits + ' ')    #Number of possible characters
-MAX_CHAR = 64           #Max # characters per block
-IS = (18, 640, 3)       #Image size for CNN
-#Architecture of the neural network
-#The input volume is reduce to the shape of the output in conv layers
-#18 / 2 * 3 * 3 = 1 and 640 / 2 * 5 = 64 output.shape
-ws = [('C', [5, 5,  3, NC // 2], [1, 2, 2, 1]), ('AF', 'relu'),     
-      ('C', [4, 4, NC // 2, NC], [1, 3, 1, 1]), ('AF', 'relu'), 
-      ('C', [3, 5, NC,      NC], [1, 3, 5, 1]), ('AF', 'relu'),
-      ('R', [-1, 64, NC])]
-#Create the neural network in TensorFlow
-cnnc = ANNC(IS, ws, batchSize = 512, learnRate = 2e-5, maxIter = 64, reg = 1e-5, tol = 1e-2, verbose = True)
-if not cnnc.RestoreModel('TFModel/', 'ocrnet'):
-    FitModel()
-else:
-    with open('TFModel/_classes.txt') as F:
-        cnnc.RestoreClasses(F.read().splitlines())
+def LoadNets():
+    CNN = [MakeNet('ocrnet' + str(i)) for i in range(NNET)]
+    if not CNN[-1].RestoreModel('TFModel/', 'ocrgraph'):
+        A, Y, T, FN = LoadData()
+        for CNNi in CNN:
+            FitModel(CNNi, A, Y, T, FN)
+        CNN[-1].SaveModel(os.path.join('TFModel', 'ocrgraph'))
+        with open('TFModel/_classes.txt', 'w') as F:
+            F.write('\n'.join(CNN[-1]._classes))
+    else:
+        with open('TFModel/_classes.txt') as F:
+            cl = F.read().splitlines()
+        for CNNi in CNN:
+            CNNi.RestoreClasses(cl)
+    return CNN
     
+CNN = LoadNets()                    #CNNs
+YHL = [CNNi.YHL for CNNi in CNN]    #Prediction placeholders
+TFS = CNN[-1].GetSes()              #Get tensorflow session
 if __name__ == "__main__":
     P = argparse.ArgumentParser(description = 'Deep learning based OCR')
     P.add_argument('-f', action = 'store_true', help = 'Force model training')
